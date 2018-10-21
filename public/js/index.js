a32dbac660cdce4064d32b65db4678dd4350b5d8
Travis Lawrence <tlawrence@tumblr.com>
2018-10-21 15:03:30 -0700

    Initial setup commit

---
+++ public/js/index.js
@@ -0,0 +1,42 @@
+class DogComponent {
+  constructor(id) {
+    this.el = document.getElementById(id);
+    this.dogs = this.requestDogs("/assets/data/dogs.json").then(({ dogs }) => {
+      dogs.forEach(dog => {
+        const dogEl = new Dog(dog.image);
+        this.el.appendChild(dogEl.render());
+      });
+    });
+  }
+
+  requestDogs(url) {
+    return new Promise((resolved, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => {
+        return resolved(JSON.parse(xhr.response));
+      };
+      xhr.onerror = () => {
+        return reject();
+      };
+      xhr.send();
+    });
+  }
+}
+
+class Dog {
+  constructor(imageUrl) {
+    this.name = "Rocko";
+    this.image = new Image();
+    this.image.src = imageUrl;
+  }
+
+  render() {
+    this.el = document.createElement("div");
+    this.el.classList = "dog";
+    this.el.appendChild(this.image);
+    return this.el;
+  }
+}
+
+new DogComponent("dogs-data");
