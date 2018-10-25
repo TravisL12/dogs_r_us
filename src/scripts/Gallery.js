af42d205befeb4e3e9736c87e6d2160ed0e3ca85
Travis Lawrence <tlawrence@tumblr.com>
2018-10-25 12:28:22 -0700

    Pagination working, think its done

--- src/scripts/Gallery.js
+++ src/scripts/Gallery.js
@@ -4,18 +4,24 @@ class Gallery {
   constructor(id, profileModal) {
     this.el = document.getElementById(id);
     this.modal = profileModal;
-    this.page = 0;
+    this.page = 1;
     this.perPage = 10;
+    this.pageInfo = document.getElementById("page-info");
 
     this.requestDogs("assets/data/dogs.json").then(({ dogs }) => {
       this.dogs = dogs.map(dog => {
         return new Dog(dog, this.modal.showProfile);
       });
+      this.maxPages = Math.ceil(this.dogs.length / this.perPage);
       this.appendDogs();
     });
 
     this.previousBtn = this.createPaginateBtn("previous-page", this.prevPage);
     this.nextBtn = this.createPaginateBtn("next-page", this.nextPage);
+  }
+
+  updatePageInfo() {
+    this.pageInfo.innerText = `Page ${this.page} of ${this.maxPages}`;
   }
 
   createPaginateBtn(id, listener) {
@@ -24,21 +30,24 @@ class Gallery {
   }
 
   prevPage() {
-    if (this.page === 0) return;
-    this.page -= 1;
-    this.appendDogs();
+    if (this.page === 1) return;
+    this.updatePage(-1);
   }
 
   nextPage() {
-    const maxPages = Math.ceil(this.dogs.length / this.perPage - 1);
-    if (this.page + 1 > maxPages) return;
-    this.page += 1;
+    if (this.page + 1 > this.maxPages) return;
+    this.updatePage(1);
+  }
+
+  updatePage(num) {
+    this.page = this.page + num;
     this.appendDogs();
   }
 
   appendDogs() {
+    this.updatePageInfo();
     this.el.innerHTML = "";
-    const range = this.page * this.perPage;
+    const range = (this.page - 1) * this.perPage;
     this.dogs.slice(range, range + 10).forEach(dog => {
       this.el.appendChild(dog.render());
     });
